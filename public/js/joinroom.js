4369d2fd26d38922e246f713f162f0edc97e9097
Keith Yong <terda12@gmail.com>
2015-02-28 10:27:03 -0500

    Basic React chatroom listing render

--- public/js/joinroom.js
+++ public/js/joinroom.js
@@ -1,23 +1,52 @@
+var App = React.createClass({
+    getInitialState: function() {
+        return {rooms: []};
+    },
+    componentDidMount: function() {
+        $.ajax({
+            url: '/chatrooms',
+            dataType: 'json',
+            success: function(data) {
+                this.setState({rooms: data});
+            }.bind(this)
+        });
+    },
+    render: function() {
+        return(
+            <div className="chatRoomListApp">
+            <ChatRoomsList rooms={this.state.rooms} />
+            </div>
+        );
+    }
+
+})
 var ChatRoomsList = React.createClass({
     render: function() {
+        var roomNodes = this.props.rooms.map(function(room) {
+            return (
+                <ChatRoom roomName={room.name}/>
+            );
+        });
         return (
-            <div className="chatRoomList">
-
-            </div>
+            <ul className="chatRoomList">
+                {roomNodes}
+            </ul>
         );
     }
 });
 
 var ChatRoom = React.createClass({
     render: function() {
+        var roomName = this.props.roomName;
         return (
-            <div>
-            </div>
+            <li className='room'>
+                <a href={'r/' + roomName}>{roomName}</a>
+            </li>
         );
     }
 });
 
 React.render(
-    <ChatRoomsList />,
+    <App />,
     document.getElementById('chatRoomList')
 );
