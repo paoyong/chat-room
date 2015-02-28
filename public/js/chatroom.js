c256573ff7a755f13a29d97ffa9db77efd9720b7
Keith Yong <terda12@gmail.com>
2015-02-28 15:00:20 -0500

    Basic chat room messageList rendering

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -10,13 +10,68 @@ socket.on('chat message', function(msg) {
     $('messages').append($('<li>').text(msg));
 });
 
+var roomName = $('#roomName').text();
+var limit = 20;
+console.log('roomName = ' + roomName);
+
 var ChatApp = React.createClass({
-    return (
-        <div className="chatApp">
-            Hello world
-        </div>
-    );
+    getInitialState: function() {
+        return {messages: []};
+    },
+    componentDidMount: function() {
+        $.ajax({
+            url: '/messages/?chatroom=' + roomName +'&limit=' + limit,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({messages: data});
+            }.bind(this),
+            failure: function(xhr, status, err) {
+                console.err(url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    render: function() {
+        return (
+            <div className="chatApp">
+                <MessagesList messages={this.state.messages}/>
+                <ChatForm />
+            </div>
+        );
+    }
 });
+
+var MessagesList = React.createClass({
+    render: function() {
+        var messageNodes = this.props.messages.map(function(msg) {
+            return (<Message msg={msg} />);
+        });
+        return (
+            <ul>
+                {messageNodes}
+            </ul>
+        );
+    }
+});
+
+var Message = React.createClass({
+    render: function() {
+        var msg = this.props.msg;
+        return (
+            <li>{msg.user}: {msg.msg}</li>
+        );
+    }
+});
+
+var ChatForm = React.createClass({
+    render: function() {
+        return (
+            <form>
+                <input type='text' placeholder='Say something...' />
+                <button type='submit'>Submit</button>
+            </form>
+        );
+    }
+})
 
 React.render(
     <ChatApp />,
--- routes/r.js
+++ routes/r.js
@@ -1,9 +1,15 @@
+// router for /r/:room
+// Also handles POST to room
 var express = require('express');
 var router = express.Router();
+var chatdb = require('../db.js');
 
 /* GET home page. */
 router.get('/:roomName', function(req, res, next) {
     res.render('room', { roomName: req.params.roomName });
 });
 
+router.post('/:roomName', function(req, res, next) {
+    
+});
 module.exports = router;
