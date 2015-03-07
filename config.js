a90c324bb1bbae55a55ac050f45f9588ac639e06
Keith Yong <terda12@gmail.com>
2015-03-07 09:35:38 -0500

    postgres url changed

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     // Our database filename
     // anything://user:password@host:port/database
     chat_db_filename: 'chat.db',
-    pg_dokku_url: 'postgres://root:UIrfnLTKQPdRwULE@172.17.42.1:49153/db',
+    pg_server_url: 'postgres://postgres@localhost:5432/chatter',
     pg_local_url: 'postgres://keithy@localhost:5432/chatter'
 };
