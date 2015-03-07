2e69156d773995ef6b5f6b2f307e893e4fdc33df
Keith Yong <terda12@gmail.com>
2015-03-07 12:01:36 -0500

    Changed pg server string on config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     // Our database filename
     // anything://user:password@host:port/database
     chat_db_filename: 'chat.db',
-    pg_server_url: 'postgres://postgres@localhost:5432/chatter',
+    pg_server_url: 'postgres://postgres:postgres@localhost:5432/chatter',
     pg_local_url: 'postgres://keithy@localhost:5432/chatter'
 };
